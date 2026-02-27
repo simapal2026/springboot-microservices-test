18e64625ba92992c2f401aaa7276a3b497ff43a1
simapal2026 <sima_pal@yahoo.com>
2026-02-27 14:55:23 +0530

    Added non standard println

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//This is the main method of the spring boot microservice
 	public static void main(String[] args) {
+		//adding non standard logging
+		System.out.println("Here is the non standard print");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
