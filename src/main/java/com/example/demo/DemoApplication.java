d268a810cc0ddb2e094ad4b83baab2b00a7fa368
simapal2026 <sima_pal@yahoo.com>
2026-02-27 13:40:56 +0530

    Added comments and Println

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    //Added comment for the main method
 	public static void main(String[] args) {
+		//added System.out which is non standard logging
+		System.out.println("Test Main");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
